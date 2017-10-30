fd89725277f7479773ce459e1b5c60f073699443
mlg1996 <32440812+mlg1996@users.noreply.github.com>
2017-10-30 16:27:31 -0500

    Version .36
    the 2D walls are now added in the assets folder. tilesheet was created and is embeded in tilemap

--- js/level1.js
+++ js/level1.js
@@ -12,6 +12,7 @@ var level1 = {
         game.load.tilemap('map1', 'assets/map1.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('terrain-assets', 'assets/terrain-assets.png');
         game.load.image('accessories_tile', 'assets/accessories_tile.png');
+        game.load.image('2dwalls', 'assets/2dwalls.png');
         game.load.image('money', 'assets/money.png');
         game.load.spritesheet('robber', 'assets/robber_two.png', 32, 48, 18);
         game.load.spritesheet('cop', 'assets/cop.png', 32, 48, 18);
@@ -29,6 +30,7 @@ var level1 = {
         // (i.e. what you saved/named the tileset as)
         map.addTilesetImage('terrain-assets');
         map.addTilesetImage('accessories_tile');
+        map.addTilesetImage('2dwalls');
 
         // the parameter is what the layer is ***called in tiled***
         Floor = map.createLayer('Floor');
